ae930e54decf17a6da263ceb272752593d92a2e7
Gavin <gavinpease@gmail.com>
2023-07-21 12:25:14 -0600

    Remove logging, and optionally removed mixins file if required

--- src/main/java/com/peasenet/mod/NoRainMod.java
+++ src/main/java/com/peasenet/mod/NoRainMod.java
@@ -7,14 +7,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class NoRainMod implements ModInitializer {
-    public static Logger LOGGER = LoggerFactory.getLogger("gavinsmod");
 
     /**
      * Runs the mod initializer.
      */
     @Override
     public void onInitialize() {
-        LOGGER.info("Initializing GEM No Rain...");
         GavinsMod.addMod(new ModNoRain());
     }
 }
